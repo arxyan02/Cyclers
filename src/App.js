a7f1f11db14a457a9765e23c8ca7120ab6151d36
arxyan02 <aryan.avnish02@gmail.com>
2023-03-07 23:21:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Logo from './logo.jpg'
+import Cycler from './cycler.jpg'
+import './App.css'
+import { IconButton } from '@mui/material';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='Topbar'>
+        <div className='Logodiv'><img className='Logo' src={Logo}></img></div>
+        <div className='Topbuttons'>
+          <button className='Cyclersbutton'><h3>Cyclers</h3></button>
+          <button className='Workingbutton'><h3>Working</h3></button>
+          <button className='Helpbutton'><h3>help</h3></button>
+          <button className='Explorebutton'><h3>Explore now</h3></button>
+        </div>
+      </div>
+        <div className='Cycler'>
+        <img className='Bike' src={Cycler}></img>
+        </div> 
     </div>
   );
 }
